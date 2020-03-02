4fbdf037e12076b7942aabf643c137b143adbe5d
outshudi <djcongojack@gmail.com>
2020-03-02 16:21:58 +0100

    rows insert into tables bara, urna, auto

---
+++ sql/preventivo.sql
@@ -0,0 +1,36 @@
+-- popolamento tabella bara
+
+insert into bara values ('1001','abete','Lara','600');
+insert into bara values ('1002','abete','Torino','610');
+insert into bara values ('1003','abete','Gianmaria','620');
+insert into bara values ('1004','Mogano','Lara','730');
+insert into bara values ('1005','Mogano','Annamaria','740');
+insert into bara values ('1006','Mogano','Luigi','750');
+insert into bara values ('1007','Mogano','Ginevra','730');
+insert into bara values ('1008','Mogano','Luna','740');
+insert into bara values ('1009','Mogano','Torino','750');
+insert into bara values ('1010','Betulla','Mario','1030');
+insert into bara values ('1011','Betulla','Luigi','1040');
+insert into bara values ('1012','Betulla','Gina','1750');
+insert into bara values ('1013','Betulla','Savona','1750');
+
+-- popolamento tabella urna
+
+insert into urna values ('2001','Marmo','Cigliega','130');
+insert into urna values ('2002','Marmo','Betulla','340');
+insert into urna values ('2003','Marmo','Larice','250');
+insert into urna values ('2004','Acciaio','Tulipano','150');
+insert into urna values ('2005','Acciaio','Torino','160');
+
+-- popolamento tabella auto
+
+insert into auto values ('3001','BMW','M3','2000','2.467');
+insert into auto values ('3002','BMW','X6','2500',' 2.998');
+insert into auto values ('3003','BMW','M8','4000',' 2.998');
+insert into auto values ('3004','Bentley','Continental','6050','3.996');
+insert into auto values ('3005','Mercedes-Benz','GLE','4000','2.996');
+insert into auto values ('3006','Mercedes-Benz','GLH','5000','3.996');
+insert into auto values ('3007','Audi','A4','2500','1.968');
+insert into auto values ('3008','Audi','A5','3000','2.894');
+insert into auto values ('3009','Audi','A6','5000','2.995');
+insert into auto values ('3010','Fiat','Torino','2000','1.968');
