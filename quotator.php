7f86ac925ce795811af387ca619f6dc0bfe87747
Alessio Bettarello <bettaz.developer@gmail.com>
2020-06-25 18:38:32 +0200

    fixed input submit

--- quotator.php
+++ quotator.php
@@ -1,9 +1,4 @@
 <?php
-session_start(['cookie_lifetime' => 86400]);
-if(!isset($_SESSION['logged']) || !$_SESSION['logged']){
-	error_log('non auth user trying to access admin, redirected to login');
-	header('Location: login.php');
-}
 $quot_file = fopen('./views/preventivatore.xhtml','r');
 $quot_content = fread($quot_file,filesize('./views/preventivatore.xhtml'));
 echo $quot_content;
