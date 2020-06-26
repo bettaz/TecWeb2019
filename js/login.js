8e891169e46a800f058d87b77d3694923286bc39
Olivier <djcongojack@gmail.com>
2020-06-26 12:15:09 +0200

    olly

--- js/login.js
+++ js/login.js
@@ -1,10 +1,33 @@
-function check(){
+window.addEventListener('load',(event)=>{
+    document.getElementById("frmLogin").addEventListener("submit",(event)=> {
+            event.preventDefault();
+            check(event.target);
+        }
+    )});
+
+function check(form){
+    let suggest = ["Username non inserito",
+        "Password non inserito"];
     let name = document.getElementById('uname').value;
     let pwd = document.getElementById('password').value;
+
+
+    console.log("function");
     if(name===""){
-        alert('Username non inserito');
+        document.getElementById('usernamep').innerHTML=suggest[0];
+
     }
-    if(pwd===""){
-        alert('Password non inserita');
+    else {
+        if(pwd===""){
+            document.getElementById('passwordp').innerHTML=suggest[1];
+
+        }
+        else {
+
+            form.submit();
+        }
     }
+
 }
+
+
--- js/preventivatore.js
+++ js/preventivatore.js
@@ -4,7 +4,25 @@ window.addEventListener('load',(event)=>{
             check(event.target);
         }
     )});
-function check0(){
+
+function check(form){
+    let suggest = ["Codice fiscale non corretto",
+        "Il nome cliente vuoto o contiene una cifra, riprova",
+        'Il cognome cliente vuoto o contiene una cifra, riprova',
+        'Il nome defunto/a vuoto o contiene una cifra, riprova',
+        'Il cognome defunto/a vuoto o contiene una cifra, riprova',
+        'La data deve avere formato AAAA-MM-GG',
+        'Il nome della via vuoto o contiene una cifra, riprova',
+        'Il nome della città vuoto o contiene una cifra, riprova',
+        'Non è stata selezionata la provincia, riprova',
+        'Il numero non inserito o contiene delle lettere, riprova',
+        'Selezionare la cerimonia desiderata, riprova',
+        'Selezionare la bara desiderata, riprova',
+        'Indicare se si desidera o meno la cremazione, riprova',
+        "Indicare l'urna desiderata, riprova",
+        "Indicare il modello di carro funebre desiderato, riprova",
+        "Indicare il tipo di composizione floreale desiderata, riprova"];
+
     let cf = document.getElementById('cf').value;
     let nomeC= document.getElementById("nomeC").value;
     let cognomeC= document.getElementById("cognomeC").value;
@@ -17,6 +35,9 @@ function check0(){
     let cell= document.getElementById("cell").value;
     let nascita = document.getElementById('nascita').value;
     let decesso = document.getElementById('decesso').value;
+
+    let cerimoniaSelect = document.getElementById('cerimonia');
+    let cerimonia = cerimoniaSelect.options[cerimoniaSelect.selectedIndex].text;
 
     let baraSelect = document.getElementById('bara');
     let bara = baraSelect.options[baraSelect.selectedIndex].text;
@@ -37,115 +58,145 @@ function check0(){
     let expChar= new RegExp('^[a-z]+$','i');
     let expNumb = new RegExp('^[0-9]+$');
     let expDate = RegExp('^\\d{4}\\-(0[1-9]|1[012])\\-(0[1-9]|[12][0-9]|3[01])$');
-
-    if(!expCf.test(cf)){
-        alert('Codice fiscale non corretto');
-    }
-
-    if(!expChar.test(nomeC)){
-        alert('Il nome cliente vuoto o contiene una cifra, riprova');
-    }
-
-    if(!expChar.test(cognomeC)){
-        alert('Il cognome cliente vuoto o contiene una cifra, riprova');
-    }
-
-    if(!expChar.test(nomeD)){
-        alert('Il nome defunto/a vuoto o contiene una cifra, riprova');
-    }
-
-    if(!expChar.test(cognomeD)){
-        alert('Il cognome defunto/a vuoto o contiene una cifra, riprova');
-    }
-
-    if(!via){
-        alert('Il nome della via vuoto o contiene una cifra, riprova');
-    }
-
-    if(!expChar.test(citta)){
-        alert('Il nome della città vuoto o contiene una cifra, riprova');
-    }
-
-    if(optionP==="---"){
-        alert('Non è stata selezionata la provincia, riprova');
-    }
-
-    if(!expNumb.test(cell)){
-        alert('Il numero non inserito o contiene delle lettere, riprova');
-    }
-
-    if(bara==='---'){
-        alert('Selezionare la bara desiderata, riprova');
-    }
-
-    if(!sicremazione && !nocremazione){
-        alert('Indicare se si desidera o meno la cremazione, riprova');
-    }
-
-    if(sicremazione && urna==='---'){
-        alert("Indicare l'urna desiderata, riprova");
-    }
-
-    if(auto==='---'){
-        alert("Indicare il modello di carro funebre desiderato, riprova");
-    }
-
-    if(fiori==='---'){
-        alert("Indicare il tipo di composizione floreale desiderata, riprova");
-    }
-}
-function check(form){
-    let suggest = ["Codice fiscale non corretto",
-        "Il cognome cliente vuoto o contiene una cifra, riprova",
-        'Il cognome cliente vuoto o contiene una cifra, riprova',
-        'Il nome defunto/a vuoto o contiene una cifra, riprova',
-        'Il cognome defunto/a vuoto o contiene una cifra, riprova',
-        'Il nome della via vuoto o contiene una cifra, riprova',
-        'Il nome della città vuoto o contiene una cifra, riprova',
-        'Non è stata selezionata la provincia, riprova',
-        'Il numero non inserito o contiene delle lettere, riprova',
-        'Selezionare la bara desiderata, riprova',
-        'Indicare se si desidera o meno la cremazione, riprova',
-        "Indicare l'urna desiderata, riprova",
-        "Indicare il modello di carro funebre desiderato, riprova",
-        "Indicare il tipo di composizione floreale desiderata, riprova"];
-
-    let cf = document.getElementById('cf').value;
-    /*let nomeC= document.getElementById("nomeC").value;
-    let cognomeC= document.getElementById("cognomeC").value;
-    let nomeD= document.getElementById("nomeD").value;
-    let cognomeD= document.getElementById("cognomeD").value;
-    let via= document.getElementById("via").value;
-    let citta= document.getElementById("citta").value;
-    let provincia= document.getElementById('provincia');
-    let optionP = provincia.options[provincia.selectedIndex].text;
-    let cell= document.getElementById("cell").value;
-    let nascita = document.getElementById('nascita').value;
-    let decesso = document.getElementById('decesso').value;
-
-    let baraSelect = document.getElementById('bara');
-    let bara = baraSelect.options[baraSelect.selectedIndex].text;
-
-    let sicremazione = document.getElementById('sicremazione').checked;
-    let nocremazione = document.getElementById('nocremazione').checked;
-
-    let urnaSelect = document.getElementById('urna');
-    let urna = urnaSelect.options[urnaSelect.selectedIndex].text;
-
-    let autoSelect = document.getElementById('auto');
-    let auto = autoSelect.options[autoSelect.selectedIndex].text;
-
-    let fioriSelect = document.getElementById('fiori');
-    let fiori = fioriSelect.options[fioriSelect.selectedIndex].text;*/
-
-    let expCf= RegExp('^[A-Z]{6}[0-9]{2}[A-Z][0-9]{2}[A-Z][0-9]{3}[A-Z]$');
-    let expChar= new RegExp('^[a-z]+$','i');
-    let expNumb = new RegExp('^[0-9]+$');
-    let expDate = RegExp('^\\d{4}\\-(0[1-9]|1[012])\\-(0[1-9]|[12][0-9]|3[01])$');
     form.removeAttribute("action");
     console.log("function");
     if(!expCf.test(cf)){
         document.getElementById('cfp').innerHTML=suggest[0];
+        form.preventDefault=true;
+    }
+    else {
+        form.setAttribute("action","quotator.php");
+        console.log()
+        form.submit();
+    }
+    if(!expChar.test(nomeC)){
+        document.getElementById('ncp').innerHTML=suggest[1];
+        form.preventDefault=true;
+    }
+    else {
+        form.setAttribute("action","quotator.php");
+        console.log()
+        form.submit();
+    }
+    if(!expChar.test(cognomeC)){
+        document.getElementById('ccp').innerHTML=suggest[2];
+        form.preventDefault=true;
+    }
+    else {
+        form.setAttribute("action","quotator.php");
+        console.log()
+        form.submit();
+    }
+    if(!expChar.test(nomeD)){
+        document.getElementById('ndp').innerHTML=suggest[3];
+        form.preventDefault=true;
+    }
+    else {
+        form.setAttribute("action","quotator.php");
+        console.log()
+        form.submit();
+    }
+    if(!expChar.test(cognomeD)){
+        document.getElementById('cdp').innerHTML=suggest[4];
+        form.preventDefault=true;
+    }
+    else {
+        form.setAttribute("action","quotator.php");
+        console.log()
+        form.submit();
+    }
+    if(!expDate.test(nascita)){
+        document.getElementById('np').innerHTML=suggest[5];
+        form.preventDefault=true;
+    }
+    else {
+        form.setAttribute("action","quotator.php");
+        console.log()
+        form.submit();
+    }
+    if(!expDate.test(decesso)){
+        document.getElementById('dp').innerHTML=suggest[5];
+        form.preventDefault=true;
+    }
+    else {
+        form.setAttribute("action","quotator.php");
+        console.log()
+        form.submit();
+    }
+    if(!via){
+        document.getElementById('vp').innerHTML=suggest[6];
+        form.preventDefault=true;
+    }
+    else {
+        form.setAttribute("action","quotator.php");
+        console.log()
+        form.submit();
+    }
+    if(!expChar.test(citta)){
+        document.getElementById('cp').innerHTML=suggest[7];
+        form.preventDefault=true;
+    }
+    else {
+        form.setAttribute("action","quotator.php");
+        console.log()
+        form.submit();
+    }
+    if(!expChar.test(provincia)){
+        document.getElementById('pp').innerHTML=suggest[8];
+        form.preventDefault=true;
+    }
+    else {
+        form.setAttribute("action","quotator.php");
+        console.log()
+        form.submit();
+    }
+    if(!expNumb.test(cerimonia)){
+        document.getElementById('clp').innerHTML=suggest[9];
+        form.preventDefault=true;
+    }
+    else {
+        form.setAttribute("action","quotator.php");
+        console.log()
+        form.submit();
+    }
+    if(cerimonia==="---"){
+        document.getElementById('cerp').innerHTML=suggest[10];
+        form.preventDefault=true;
+    }
+    else {
+        form.setAttribute("action","quotator.php");
+        console.log()
+        form.submit();
+    }
+    if(bara==="---"){
+        document.getElementById('barap').innerHTML=suggest[11];
+        form.preventDefault=true;
+    }
+    else {
+        form.setAttribute("action","quotator.php");
+        console.log()
+        form.submit();
+    }
+    if(urna==="---"){
+        document.getElementById('up').innerHTML=suggest[13];
+        form.preventDefault=true;
+    }
+    else {
+        form.setAttribute("action","quotator.php");
+        console.log()
+        form.submit();
+    }
+    if(auto==="---"){
+        document.getElementById('autop').innerHTML=suggest[14];
+        form.preventDefault=true;
+    }
+    else {
+        form.setAttribute("action","quotator.php");
+        console.log()
+        form.submit();
+    }
+    if(fiori==="---"){
+        document.getElementById('fp').innerHTML=suggest[15];
         form.preventDefault=true;
     }
     else {
