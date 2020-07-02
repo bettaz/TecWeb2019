cd3142675053301a65b6843edaf912bc856f4ddc
Alessio Bettarello <bettaz.developer@gmail.com>
2020-07-02 21:15:00 +0200

    clean & fix js checks

--- js/login.js
+++ js/login.js
@@ -1,33 +1,51 @@
-window.addEventListener('load',(event)=>{
-    document.getElementById("frmLogin").addEventListener("submit",(event)=> {
-            event.preventDefault();
-            check(event.target);
-        }
-    )});
-
+window.onload = event =>{
+    document.getElementById("frmLogin").onsubmit = event => {
+        event.preventDefault();
+        check(event.target);
+    };
+};
+const suggestions = {
+    "uname": "Inserire l'username",
+    "password": "Inserire la password"
+};
 function check(form){
-    let suggest = ["Username non inserito",
-        "Password non inserito"];
     let name = document.getElementById('uname').value;
     let pwd = document.getElementById('password').value;
-
-
-    console.log("function");
-    if(name===""){
-        document.getElementById('usernamep').innerHTML=suggest[0];
-
+    let tag = "";
+    if(pwd === "" || name ===""){
+        if(name==="")
+            tag="uname";
+        else
+            tag="password";
+        let errors = document.createElement("div");
+        errors.id = "errors";
+        errors.className = "linea";
+        errors.tabIndex=0;
+        let paragraph = document.createElement("p");
+        let anchor = document.createElement("a");
+        anchor.tabIndex = 0;
+        anchor.onclick = () => clickFocus(tag);
+        anchor.onkeyup = event => keyFocus(event,tag);
+        anchor.innerText = suggestions[tag];
+        paragraph.appendChild(anchor);
+        errors.appendChild(paragraph);
+        if (document.getElementById("errors"))
+            document.getElementById("errors").remove();
+        document.getElementById("content").insertBefore(errors,form);
+        errors.focus();
     }
     else {
-        if(pwd===""){
-            document.getElementById('passwordp').innerHTML=suggest[1];
-
-        }
-        else {
-
-            form.submit();
-        }
+        form.submit();
     }
-
+}
+function clickFocus(tagName) {
+    document.getElementById(tagName).focus();
+}
+function keyFocus(keyEvent, tagName) {
+    if(keyEvent.key == "Enter"){
+        keyEvent.preventDefault();
+        clickFocus(tagName);
+    }
 }
 
 
