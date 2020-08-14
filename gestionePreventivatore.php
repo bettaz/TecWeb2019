a6cbb71d7aebf7ad31708969c75fd9866c378a51
Gotta96 <mattia.gottardello@studenti.unipd.it>
2020-08-14 12:59:33 +0200

    added sub structure

---
+++ gestionePreventivatore.php
@@ -0,0 +1,10 @@
+<?php
+session_start(['cookie_lifetime' => 86400]);
+if(!isset($_SESSION['logged']) || !$_SESSION['logged']){
+	error_log('non auth user trying to access admin, redirected to login');
+	header('Location: login.php');
+}
+
+$usr_mng_file= fopen('views/gestionePreventivatore.xhtml','r');
+$mng_content = fread($usr_mng_file,filesize('views/gestionePreventivatore.xhtml'));
+echo $mng_content;
